0e10f523449a356afb698f9e1f82d72beec9530d
김예진 <wlsdong0004@gmail.com>
2021-05-06 18:50:26 +0900

    Create UI

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -4,6 +4,14 @@ import './App.css';
 const App: FC = () => {
   return (
     <div className="App">
+      <div className="Header">
+        <div className="InputContainer">
+          <input type="text" placeholder=""></input>
+          <input type="number" placeholder="DeadLine"></input>
+        </div>
+        <button>추가하기</button>
+      </div>
+      <div className="todolist"></div>
     </div>
   );
 };
