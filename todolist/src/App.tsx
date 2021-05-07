dad7d4fc4552c18a464e4c9207d0b3217ad728fb
김예진 <wlsdong0004@gmail.com>
2021-05-07 15:16:40 +0900

    Receive input

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,15 +1,38 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC, useState } from "react";
 import './App.css';
+import {ITask} from './interface'
 
 const App: FC = () => {
+
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const hadleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    console.log(event.target.name);
+    if (event.target.name === "taskName"){
+      console.log(event.target.value)
+      setTask(event.target.value);
+    } else {
+      console.log(event.target.value);
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    console.log(todoList);
+  };
+
   return (
     <div className="App">
       <div className="Header">
         <div className="InputContainer">
-          <input type="text" placeholder=""></input>
-          <input type="number" placeholder="DeadLine"></input>
+          <input type="text" placeholder="" onChange={hadleChange } name="taskName"></input>
+          <input type="number" placeholder="DeadLine" onChange={hadleChange}></input>
         </div>
-        <button>추가하기</button>
+        <button onClick={addTask}>추가하기</button>
       </div>
       <div className="todolist"></div>
     </div>
